58c5c22926332e71a39b2d5f655341dc79a4708a
Ninniet5670 <marconeto3000@gmail.com>
2022-10-20 14:09:29 -0300

    added functionalities

--- data_processing.py
+++ data_processing.py
@@ -1,21 +1,38 @@
 from collections import Counter
 
-erros = []
 
-with open('pep8_errors.txt', 'r+') as arq:
-    arqq = arq.read()
+def pep8_data_process():
+    erros = []
+
+    with open('pep8_errors.txt', 'r+') as arq:
+        arqq = arq.read()
+
     arqq = arqq.split('\n')
     arqq.pop()
     for i, erro in enumerate(arqq):
         arqq[i] = str(erro.split(': ')[1:][0]).split(',')
 
-for erro in arqq:
-    try:
-        x, y = erro[0].split()[0], int(erro[1].split()[-1])
-    except:
-        x, y = erro[0].split()[0], 1
-    
-    for i in range(y):
-        erros.append(x)
+    for erro in arqq:
+        try:
+            x, y = erro[0].split()[0], int(erro[1].split()[-1])
+        except:
+            x, y = erro[0].split()[0], 1
+        
+        for i in range(y):
+            erros.append(x)
 
-print(dict(Counter(erros)))
+    return dict(Counter(erros))
+
+
+def pylint_data_process():
+    with open('pylint_errors.txt', 'r+') as arq:
+        arqq = arq.read()
+
+    arqq = arqq.split('\n')
+    for i, erro in enumerate(arqq):
+        if '*' in arqq[i] or arqq[i] == '':
+            arqq.pop(i)
+
+    print(arqq)
+
+pylint_data_process()
--- main.py
+++ main.py
@@ -1,40 +1,52 @@
 import os
 
 
-arq = open('pep8_errors.txt', 'a')
+pep8_errors = open('pep8_errors.txt', 'a')
+pylint_errors = open('pylint_errors.txt', 'a')
 
 
-def recursive_dir(code):
+def recursive_dir():
     for file in os.listdir(os.curdir):
         if os.path.isdir(file):
             os.chdir(file)
-            recursive_dir(code)
+            recursive_dir()
         else:
             if file.split('.')[-1] == 'py':
+                # pep 8
                 os.system(
-                    f'{code} {file} >> auxiliar.txt')
+                    f"pycodestyle --first {file} >> auxiliar.txt")
 
                 with open('auxiliar.txt', 'r') as arqq:
-                    arq.write(arqq.read())
+                    pep8_errors.write(arqq.read())
+                os.remove('auxiliar.txt')
+
+                # pylint
+                os.system(
+                    f"pylint --msg-template='{{msg_id}}:{{category}}' {file} >> auxiliar.txt")
+
+                with open('auxiliar.txt', 'r') as arqq:
+                    pylint_errors.write(arqq.read())
                 os.remove('auxiliar.txt')
     os.chdir('..')
 
 
-choice = input('''
-Escolha uma opção:
-[1] Checar erros de pep8
-[2] Checar erros de pylint
-[3] Rodar autopep recursivamente no diretório atual
-[0] Sair
-Digite: ''')
+# choice = input('''
+# Escolha uma opção:
+# [1] Checar erros de pep8
+# [2] Checar erros de pylint
+# [3] Rodar autopep recursivamente no diretório atual
+# [0] Sair
+# Digite: ''')
 
-if choice == '1':
-    recursive_dir('pycodestyle --first')
-elif choice == '2':
-    recursive_dir('pylint')
-elif choice == '3':
-    recursive_dir('autopep8 --in-place --aggressive --aggressive')
-elif choice == '0':
-    exit()
+# if choice == '1':
+#     recursive_dir('pycodestyle --first')
+# elif choice == '2':
+#     recursive_dir('pylint')
+# elif choice == '3':
+#     recursive_dir('autopep8 --in-place --aggressive --aggressive')
+# elif choice == '0':
+#     exit()
+recursive_dir()
 
-arq.close()
+pep8_errors.close()
+pylint_errors.close()
