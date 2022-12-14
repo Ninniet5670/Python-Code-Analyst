337d18962218da0262e1c77688d2e82be42e221c
Ninniet5670 <marconeto3000@gmail.com>
2022-12-13 21:02:18 -0300

    added option to pep8

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ from data_processing import PySmells
 import os
 
 
-# para mais de um projeto
-# for file in os.listdir(os.curdir):
-#     if os.path.isdir(file) and file != '.git':
-#         os.system(f"pylint --msg-template='{{msg_id}}:{{msg}}' {file} >> {file}_pylint_errors.txt")
-#         os.system(f"pycodestyle --statistics -qq {file} >> {file}_pep8_errors.txt")
-
-
 os.system(f"pylint --msg-template='{{msg_id}}::{{msg}}' teste >> pylint_errors.txt")
 os.system(f"pycodestyle --statistics -qq teste >> pep8_errors.txt")
 
 print(PySmells('pylint_errors.txt').insigth_pylint())
 print(PySmells('pylint_errors.txt').insigth_pylint(True))
 print(PySmells('pylint_errors.txt').pylint_score())
+print(PySmells('pep8_errors.txt').pep8_insigth())
