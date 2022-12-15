3ad4b4ad8e5827bd2d59b1f37a4ac372806769c4
PROENÇA, Marco <54338596+Ninniet5670@users.noreply.github.com>
2022-12-15 15:31:00 -0300

    mypy added & prettier output & dir choosable

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from data_processing import PySmells
 import os
 
+def centralize(word):
+    print(f'{word:-^40}')
 
-os.system(f"pylint --msg-template='{{msg_id}}::{{msg}}' teste >> pylint_errors.txt")
-os.system(f"pycodestyle --statistics -qq teste >> pep8_errors.txt")
+dir = input('The dir to be analyzed: ')
 
+os.system(f"pylint --msg-template='{{msg_id}}::{{msg}}' {dir} > pylint_errors.txt")
+os.system(f"pycodestyle --statistics -qq {dir} > pep8_errors.txt")
+os.system(f"mypy --check-untyped-defs {dir} > mypy_errors.txt")
+
+centralize(' Pylint Errors ')
 print(PySmells('pylint_errors.txt').insigth_pylint())
+
+centralize(' Pylint Errors by Type ')
 print(PySmells('pylint_errors.txt').insigth_pylint(True))
+
+centralize(' Pylint Program Score ')
 print(PySmells('pylint_errors.txt').pylint_score())
+
+centralize(' Pep8 Errors ')
 print(PySmells('pep8_errors.txt').pep8_insigth())
