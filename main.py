913799a6fa2b5c35b2880b3d23c079ffd0973570
Ninniet5670 <marconeto3000@gmail.com>
2022-12-28 18:37:45 -0300

    insigth_pylint name changed & mypy function added

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ os.system(f"pycodestyle --statistics -qq {dir} > pep8_errors.txt")
 os.system(f"mypy --check-untyped-defs {dir} > mypy_errors.txt")
 
 centralize(' Pylint Errors ')
-print(PySmells('pylint_errors.txt').insigth_pylint())
+print(PySmells('pylint_errors.txt').pylint_insigth())
 
 centralize(' Pylint Errors by Type ')
-print(PySmells('pylint_errors.txt').insigth_pylint(True))
+print(PySmells('pylint_errors.txt').pylint_insigth(True))
 
 centralize(' Pylint Program Score ')
 print(PySmells('pylint_errors.txt').pylint_score())
 
 centralize(' Pep8 Errors ')
 print(PySmells('pep8_errors.txt').pep8_insigth())
+
+centralize(' MyPy Errors ')
+print(PySmells('mypy_errors.txt').mypy_insigth())
