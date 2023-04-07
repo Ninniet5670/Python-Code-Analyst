89fbb080f3e616d67a093d2bcccf1b60d28521b9
Marcos de Sousa <desousa.mpa@gmail.com>
2023-04-07 19:42:38 -0300

    A new version of pysmells containing python code analysis using pylint and mypy

--- README.md
+++
@@ -1,1 +0,0 @@
-# Pysmells é um pacote Python que identifica e gera relatórios de erros no seu código Python
---
+++ test/badcod.py
@@ -0,0 +1,7 @@
+import typing
+
+def ok(tt: int)->int:
+    return tt
+
+def o2(tt: int)->bool:
+    return tt
